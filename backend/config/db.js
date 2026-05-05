c64f6fc32b590fb491920284a4658a8630b2554a
BaranDogrular <b47409639@gmail.com>
2026-05-05 15:00:26 +0300

    fix database connection for render

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,11 +6,11 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 export default pool;
